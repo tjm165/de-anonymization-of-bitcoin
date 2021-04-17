79b33b7c85663d691121454698d1906ac366c6e0
dallan <dallangoldblatt@gmail.com>
2021-04-17 17:46:58 -0400

    add cluster merging and plotting example

--- cluster.py
+++ cluster.py
@@ -12,61 +12,52 @@ def cluster(transactions, heuristic, threshold=None):
     Each cluster is a list of addresses
     """
 
-    # create initial clusters through the heuristic
     clusters = []
 
     if heuristic == heuristics.shadow:
+        # Shadow heuristic relies on meta-data across multiple transactions
         address_to_merchant_count = heuristics.__get_shadow_data(transactions)
         for t in transactions:
             clusters.append(heuristics.shadow(
                 t, address_to_merchant_count, threshold))
     else:
+        # All other heursitics cluster based on current transaction only
+        seen_addresses = set()
         for t in transactions:
-            clusters.append(heuristic(t))
+            new_cluster = set(pair.address for pair in heuristic(t))
+            single_addresses = set(pair.address for pair in t.inputs + t.outputs).difference(new_cluster)
 
-    # join clusters
-    address_to_cluster = {}
-    new_cluster_id = 0
-    for cluster in clusters:
-        new_cluster_id += 1  # we might use this id or we might pass up on it
-        found_join = False
+            # Clustered addresses are added to the list of clusters
+            # Each address is marked as "seen"
+            if new_cluster:
+                clusters.append(new_cluster)
+                for address in new_cluster:
+                    seen_addresses.add(address)
 
-        i = 0
-        while not found_join and i < len(cluster):
-            address = cluster[i].address
-            if address in address_to_cluster.keys():
-                found_join = True
-                cluster_id = address_to_cluster[address]
-            i += 1
-
-        if not found_join:
-            new_cluster_id += 1
-            cluster_id = new_cluster_id
-
-        for obj in cluster:
-            address = obj.address
-            address_to_cluster[address] = cluster_id
-
-    return __address_to_cluster_dict_to_lists(address_to_cluster)
-
-
-def __address_to_cluster_dict_to_lists(address_to_cluster):
-    """
-    address_to_cluster should be {address1: cluster_id, address2: cluster_id....}
-
-    this will be converted to [[addresses in cluster_id1], [addresses in cluster_id2] ..... ]
-    """
-
-    cluster_to_address = {}
-
-    for key in address_to_cluster.keys():
-        cluster_id = address_to_cluster[key]
-        if cluster_id in cluster_to_address:
-            cluster_to_address[cluster_id].append(key)
-        else:
-            cluster_to_address[cluster_id] = [key]
-
-    return list(cluster_to_address.values())
+            # All unseen, unclustered addresses enter their own cluster
+            for address in single_addresses:
+                if address not in seen_addresses:
+                    clusters.append({address})
+                    seen_addresses.add(address)
+        
+        # Overlap between clusters likely exists - need to merge
+        to_merge = True
+        while to_merge:
+            to_merge = False
+            new_clusters = []
+            while clusters:
+                common, rest = clusters[0], clusters[1:]
+                clusters = []
+                for x in rest:
+                    if x.isdisjoint(common):
+                        clusters.append(x)
+                    else:
+                        to_merge = True
+                        common |= x
+                new_clusters.append(common)
+            clusters = new_clusters
+            
+    return clusters
 
 
 def visualize_clusters(clusters):
