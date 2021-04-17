a8ce240f34a3d20c3577e141b1a3241465c0f46b
Thomas M <tjm165@case.edu>
2021-04-17 14:07:14 -0400

    cluster vizualization

--- cluster.py
+++ cluster.py
@@ -1,10 +1,14 @@
 import heuristics
+import matplotlib.pyplot as plt
 
 
 def cluster(transactions, heuristic, threshold=None):
     """
     transactions is an array 
     heuristics is a function. See heuristics.py for all functions
+
+    returns a list of clusters
+    Each cluster is a list of addresses
     """
 
     clusters = []
@@ -19,3 +23,25 @@ def cluster(transactions, heuristic, threshold=None):
             clusters.append(heuristic(t))
 
     return clusters
+
+
+def visualize_clusters(clusters):
+    fig = plt.figure()
+    ax = fig.add_axes([0, 0, 1, 1])
+    data = __get_clusters_data(clusters)
+    ax.bar(data['cluster_ids'], data['cluster_sizes'])
+    plt.show()
+
+
+def __get_clusters_data(clusters):
+    """
+    This data is intended to be used for plotting and for comparison metrics
+    """
+
+    ids = range(len(clusters))
+    sizes = []
+
+    for cluster in clusters:
+        sizes.append(len(cluster))
+
+    return {'cluster_ids': ids, 'cluster_sizes': sizes}
